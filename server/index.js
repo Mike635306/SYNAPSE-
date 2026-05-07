346ff4ae7bcd422bf9b69dfd421a5b909ebafcc6
Mixail <mike.549382663@gmail.com>
2026-05-07 23:07:48 +0300

    update

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const port = process.env.PORT || 3000;
+const port = 3000 || process.env.PORT;
 const { Server } = require("socket.io");
 const { createServer } = require("http");
 
